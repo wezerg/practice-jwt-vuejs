8c6f523850cf05392310eb39234545586c875017
wezerg <vivian.grimaux@hotmail.fr>
2022-06-02 11:40:58 +0200

    Refacto premier composant Compteur.vue & DemoJWT.vue

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,11 @@
 <script setup>
-import { ref } from 'vue';
-import * as jose from 'jose';
-import axios from 'axios';
-
-    const title = ref('Hello World !');
-    const compteur = ref(0);
-    setInterval(() => {
-        compteur.value++;
-    }, 1000);
-
-    function resetCompteur(){
-        compteur.value = 0;
-    }
-
-    const token = ref("");
-    const tokenDecoded = ref("");
-    const tokenSaved = ref("");
-    const tokenFromAPI = ref("");
-    function decodeToken(){
-        if (token.value) {
-            const claims = jose.decodeJwt(token.value);
-            tokenDecoded.value = claims;
-        }
-    }
-    function saveToken(){
-        if (token.value) {
-            localStorage.setItem('cours-token', token.value);
-            tokenSaved.value = token.value;
-        }
-    }
-    if (localStorage.getItem('cours-token')) {
-        tokenSaved.value = localStorage.getItem('cours-token');
-    }
-    async function getTokenFromAPI(){
-        if (tokenSaved.value) {
-            const instance = axios.create({headers: {"X-Auth-Token": tokenSaved.value}});
-            const response = await instance.post('http://localhost:3001/loginToken').then(res => res).catch(err => err);
-            tokenFromAPI.value = response.request.response;
-        }
-    }
-    getTokenFromAPI();
+import Compteur from './components/Compteur.vue';
+import DemoJWT from './components/DemoJWT.vue';
 </script>
 
 <template>
-    <h1>{{title}}</h1>
-    <p>Compteur : {{compteur}}</p>
-    <input type="number" v-model="compteur">
-    <button @click="resetCompteur">Reset</button>
-    <hr/>
-    <label>Décoder mon token :</label>
-    <input type="text" v-model="token">
-    <button v-on:click="decodeToken">Décodage</button>
-    <button v-on:click="saveToken">Sauvegarder ce token</button>
-    <p>Mon token décodé : {{tokenDecoded}}</p>
-    <hr/>
-    <label>Mon token sauvegardé :</label>
-    <p>{{tokenSaved}}</p>
-    <hr/>
-    <label>Mon token de l'API :</label>
-    <p>{{tokenFromAPI}}</p>
+    <Compteur></Compteur>
+    <DemoJWT></DemoJWT>
 </template>
 
 <style>
---
+++ src/components/DemoJWT.vue
@@ -0,0 +1,46 @@
+<script setup>
+    import { ref } from 'vue';
+    import * as jose from 'jose';
+    import axios from 'axios';
+
+    const token = ref("");
+    const tokenDecoded = ref("");
+    const tokenSaved = ref("");
+    const tokenFromAPI = ref("");
+    function decodeToken(){
+        if (token.value) {
+            const claims = jose.decodeJwt(token.value);
+            tokenDecoded.value = claims;
+        }
+    }
+    function saveToken(){
+        if (token.value) {
+            localStorage.setItem('cours-token', token.value);
+            tokenSaved.value = token.value;
+        }
+    }
+    if (localStorage.getItem('cours-token')) {
+        tokenSaved.value = localStorage.getItem('cours-token');
+    }
+    async function getTokenFromAPI(){
+        if (tokenSaved.value) {
+            const instance = axios.create({headers: {"X-Auth-Token": tokenSaved.value}});
+            const response = await instance.post('http://localhost:3001/loginToken').then(res => res).catch(err => err);
+            tokenFromAPI.value = response.request.response;
+        }
+    }
+    getTokenFromAPI();
+</script>
+<template>
+    <label>Décoder mon token :</label>
+    <input type="text" v-model="token">
+    <button v-on:click="decodeToken">Décodage</button>
+    <button v-on:click="saveToken">Sauvegarder ce token</button>
+    <p>Mon token décodé : {{tokenDecoded}}</p>
+    <hr/>
+    <label>Mon token sauvegardé :</label>
+    <p>{{tokenSaved}}</p>
+    <hr/>
+    <label>Mon token de l'API :</label>
+    <p>{{tokenFromAPI}}</p>
+</template>
