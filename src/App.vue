be7e1664efbbe56450c648c437b306d75468da4b
wezerg <vivian.grimaux@hotmail.fr>
2022-06-02 10:00:40 +0200

    Ajout d'un compteur qui s'incrémente

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
 import { ref } from 'vue';
 
     const title = ref('Hello World !');
+    const compteur = ref(0);
+    setInterval(() => {
+        compteur.value++;
+    }, 1000);
 </script>
 
 <template>
     <h1>{{title}}</h1>
+    <p>Compteur : {{compteur}}</p>
 </template>
 
 <style>
