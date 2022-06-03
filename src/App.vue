6d437734cfddb2eee66fa213ea5ccab9f750305d
wezerg <vivian.grimaux@hotmail.fr>
2022-06-03 10:02:08 +0200

    Fix use connect from app

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 import Header from './components/Header.vue';
 import { useUserStore } from './services/userstore';
     
-    const {user} = useUserStore();
+    const {connect} = useUserStore();
     if (localStorage.getItem('cours-user-name')) {
-        user.value = {name: localStorage.getItem('cours-user-name')}
+        connect(localStorage.getItem('cours-user-name'));
     }
 </script>
 
