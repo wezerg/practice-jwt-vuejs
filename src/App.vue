0c07592d332afd3a90bed32a3833b472ce715224
wezerg <vivian.grimaux@hotmail.fr>
2022-06-03 15:18:22 +0200

    Récupération token en login Sauvegarde du token dans le localstorage Auto connexion si token détecté

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
 import Header from './components/Header.vue';
 import { useUserStore } from './services/userstore';
     
-    const {connect} = useUserStore();
-    if (localStorage.getItem('cours-user-name')) {
-        connect(localStorage.getItem('cours-user-name'));
-    }
-    /* eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNjU0MjQ5NzM4fQ.bJiWPMDRefVAIQvYSfx4pRS4gcx3cyS0bXZJAz008GM */
+    const {autoConnect} = useUserStore();
+    autoConnect();
+    
 </script>
 
 <template>
--- src/services/userstore.js
+++ src/services/userstore.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 import { ref } from "vue";
 
+
+axios.interceptors.request.use(function (config) {
+    config.headers['x-auth-token'] = localStorage.getItem('cours-token') || "";
+    return config;
+});
+
 axios.interceptors.response.use(function (response) {
-    console.log(response)
-    /*console.log(response.config.headers['x-auth-token']);
-    localStorage.setItem('cours-token', response.config.headers['x-auth-token']);*/
     return response;
 });
 
 const user = ref(null);
 
 function useUserStore(){
-    return {user, connect, disconnect};
+    return {user, connect, disconnect, autoConnect};
 }
 async function connect(email, password){
     if (email && password) {
@@ -19,10 +22,21 @@ async function connect(email, password){
         if (response.status !== 200) {
             return null;
         }
-        return user.value = response.data;
+        localStorage.setItem('cours-token', response.headers['x-auth-token']);
+        return user.value = response.data.user;
     }
     else{
         return null;
+    }
+}
+async function autoConnect(){
+    if (localStorage.getItem('cours-token')) {
+        const response = await axios.post('http://localhost:3001/moncompte').then(res => res).catch(err => err);
+        if (response.status !== 200) {
+            localStorage.removeItem('cours-token');
+            return null;
+        }
+        user.value = response.data.user;
     }
 }
 function disconnect(){
